9d629e5459386970e0c89421b33226fe25b0b4e5
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-12-02 18:38:44 -0500

    16-219

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Recipe } from './recipe.model';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
   encapsulation: ViewEncapsulation.None,
-  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
   constructor() { }
