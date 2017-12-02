14657c1cdf941e30f76c4a9f15cc3986b547266a
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-12-02 17:33:52 -0500

    16-210

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,16 +12,32 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params.id;
         this.editMode = params.id !== null;
+        this.initForm();
       }
     );
   }
 
+  private initForm() {
+    const recipe = this.editMode
+      ? this.recipeService.getRecipe(this.id)
+      : { name: '', imagePath: '', description: '' };
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipe.name),
+      'imagePath': new FormControl(recipe.imagePath),
+      'description': new FormControl(recipe.description),
+    });
+  }
 }
