81ae1add322369560dc61344aecb48dbebc70b90
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-12-02 16:43:59 -0500

    16-206

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -38,7 +38,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const { name, amount } = form.value;
     const ingredient = new Ingredient(name, amount);
     if (this.editMode) {
@@ -46,5 +46,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(ingredient);
     }
+
+    form.reset();
+    this.editMode = false;
   }
 }
